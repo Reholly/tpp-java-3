2f95021e1b79f8fdb308580c1a5a084ea130cc15
Reholly <eu.ustiantsev@yandex.ru>
2024-02-06 00:09:59 +0500

    Initial commit

---
+++ src/main/java/com/example/tppjava3/DownloadServlet.java
@@ -0,0 +1,20 @@
+package com.example.tppjava3;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/Download"})
+public class DownloadServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("Download.jsp").forward(request, response);
+
+    }
+
+    public void destroy() {
+    }
+}
